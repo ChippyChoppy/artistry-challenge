55862e2716c6563e1fa8cc5a8032bda90a8d77ee
ChippyChoppy <rebeccamargaret13@gmail.com>
2020-11-02 14:22:31 -0500

    done i think

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,2 +1,32 @@
 class ArtistsController < ApplicationController
+    
+    
+    def index
+        @artists = Artist.all 
+    end
+
+    def show
+        @artist = Artist.find(params[:id])
+    end
+
+    def new
+        @artist = Artist.new
+    end
+
+    def create
+        artist = Artist.create(artist_params)
+        if artist.valid?
+            redirect_to artist_path(artist)
+        else
+            "Input not valid. Try again!"
+            redirect_to new_artist_path
+        end
+    end
+
+    private
+    def artist_params
+        params.require(:artist).permit(:name, :title, :age)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  
+  resources :artist_instruments, only: [:new, :create]
+  resources :artists, :instruments, only: [:show, :index, :new, :create]
 end
